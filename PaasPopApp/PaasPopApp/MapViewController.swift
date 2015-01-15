99885d6d3998a6d74804fb50a070d6dccc3d1166
Wouter Habets <wouterhabets@gmail.com>
2015-01-15 12:59:53 +0100

    Added better map

--- PaasPopApp/PaasPopApp/MapViewController.swift
+++ PaasPopApp/PaasPopApp/MapViewController.swift
@@ -10,26 +10,57 @@ import UIKit
 
 class MapViewController: UIViewController {
 
+    @IBOutlet var scrollView: UIScrollView!
+    var imageView: UIImageView!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        // 1
+        let image = UIImage(named: "example.jpg")!
+        imageView = UIImageView(image: image)
+        imageView.frame = CGRect(origin: CGPoint(x: 0, y: 0), size:image.size)
+        scrollView.addSubview(imageView)
+        
+        // 2
+        scrollView.contentSize = image.size
+        
+        // 4
+        let scrollViewFrame = scrollView.frame
+        let scaleWidth = scrollViewFrame.size.width / scrollView.contentSize.width
+        let scaleHeight = scrollViewFrame.size.height / scrollView.contentSize.height
+        let minScale = min(scaleWidth, scaleHeight);
+        scrollView.minimumZoomScale = minScale;
+        
+        // 5
+        scrollView.maximumZoomScale = 1.0
+        scrollView.zoomScale = minScale
+        
+        centerScrollViewContents()
+        
+        
+    }
+    
+    func centerScrollViewContents() {
+        let boundsSize = scrollView.bounds.size
+        var contentsFrame = imageView.frame
+        
+        if contentsFrame.size.width < boundsSize.width {
+            contentsFrame.origin.x = (boundsSize.width - contentsFrame.size.width) / 2.0
+        } else {
+            contentsFrame.origin.x = 0.0
+        }
+        
+        if contentsFrame.size.height < boundsSize.height {
+            contentsFrame.origin.y = (boundsSize.height - contentsFrame.size.height) / 2.0
+        } else {
+            contentsFrame.origin.y = 0.0
+        }
+        
+        imageView.frame = contentsFrame
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
     }
-    
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
 }

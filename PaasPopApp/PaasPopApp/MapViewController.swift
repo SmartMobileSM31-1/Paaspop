69ebe9384904194bcffd27f5dfc8496711e8941d
geertberkers <geertberkers@hotmail.com>
2015-01-29 09:54:18 +0100

    Zoom

--- PaasPopApp/PaasPopApp/MapViewController.swift
+++ PaasPopApp/PaasPopApp/MapViewController.swift
@@ -8,23 +8,50 @@
 
 import UIKit
 
-class MapViewController: UIViewController {
+class MapViewController: UIViewController, UIScrollViewDelegate {
 
     @IBOutlet var scrollView: UIScrollView!
     var imageView: UIImageView!
+    var image: UIImage!
+    
+  /*  override func viewWillTransitionToSize(size: CGSize, withTransitionCoordinator coordinator: UIViewControllerTransitionCoordinator) {
+        if UIDevice.currentDevice().orientation.isLandscape.boolValue {
+            //UIScrollView.animateWithDuration(1.0, animations:{ self.imageView.transform = CGAffineTransformMakeRotation((90.0 * CGFloat(M_PI))/180)})
+        }
+        else
+        {
+            UIScrollView.animateWithDuration(1.0, animations:{ self.imageView.transform = CGAffineTransformMakeRotation((90.0 * CGFloat(M_PI))/180)})
+        }
+    }*/
     
     
     override func viewDidLoad() {
         super.viewDidLoad()
+
         // 1
-        let image = UIImage(named: "example.jpg")!
+        if(UIDeviceOrientationIsLandscape(UIDevice.currentDevice().orientation))
+        {
+            image = UIImage(named: "example.jpg")!
+        }
+        else
+        {
+            image = UIImage(named: "example copy.jpg")!
+        }
         imageView = UIImageView(image: image)
         imageView.frame = CGRect(origin: CGPoint(x: 0, y: 0), size:image.size)
         scrollView.addSubview(imageView)
         
+        
+        
         // 2
         scrollView.contentSize = image.size
         
+        //3
+        var doubleTapRecognizer = UITapGestureRecognizer(target: self, action: "scrollViewDoubleTapped:")
+        doubleTapRecognizer.numberOfTapsRequired = 2
+        doubleTapRecognizer.numberOfTouchesRequired = 1
+        scrollView.addGestureRecognizer(doubleTapRecognizer)
+
         // 4
         let scrollViewFrame = scrollView.frame
         let scaleWidth = scrollViewFrame.size.width / scrollView.contentSize.width
@@ -39,6 +66,27 @@ class MapViewController: UIViewController {
         centerScrollViewContents()
         
         
+    }
+
+    func scrollViewDoubleTapped(recognizer: UITapGestureRecognizer) {
+            // 1
+        let pointInView = recognizer.locationInView(imageView)
+ 
+        // 2
+        var newZoomScale = scrollView.zoomScale * 1.5
+        newZoomScale = min(newZoomScale, scrollView.maximumZoomScale)
+ 
+        // 3
+        let scrollViewSize = scrollView.bounds.size
+        let w = scrollViewSize.width / newZoomScale
+        let h = scrollViewSize.height / newZoomScale
+        let x = pointInView.x - (w / 2.0)
+        let y = pointInView.y - (h / 2.0)
+ 
+        let rectToZoomTo = CGRectMake(x, y, w, h);
+ 
+        // 4
+        scrollView.zoomToRect(rectToZoomTo, animated: true)
     }
     
     func centerScrollViewContents() {
@@ -62,5 +110,13 @@ class MapViewController: UIViewController {
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
+    }
+    
+    func viewForZoomingInScrollView(scrollView: UIScrollView!) -> UIView! {
+        return imageView
+    }
+    
+    func scrollViewDidZoom(scrollView: UIScrollView!) {
+        centerScrollViewContents()
     }
 }

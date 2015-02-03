e88e6bac4a64ec3cd433fdf04bc9cb4252658853
geertberkers <geertberkers@hotmail.com>
2015-02-03 16:24:09 +0100

    AddFriendViewController update

---
+++ PaasPopApp/PaasPopApp/AddFriendViewController.swift
@@ -0,0 +1,131 @@
+//
+//  AddFriendsViewController.swift
+//  PaasPopApp
+//
+//  Created by Wouter Habets on 29-01-15.
+//  Copyright (c) 2015 Maiko Hermans. All rights reserved.
+//
+
+import UIKit
+import AVFoundation
+
+class AddFriendViewController: UIViewController, AVCaptureMetadataOutputObjectsDelegate {
+    
+    var friends: [Person]?
+    var captureSession:AVCaptureSession?
+    var videoPreviewLayer:AVCaptureVideoPreviewLayer?
+    var qrCodeFrameView:UIView?
+    @IBOutlet var qrcodefriend: UILabel!
+    
+    @IBAction func addFriendAction(sender: UIBarButtonItem) {
+        var f: [Person] = getFriends()
+        f.append(Person(nameInput: qrcodefriend.text!))
+        setFriends(f)
+        self.navigationController?.popViewControllerAnimated(true)
+    }
+    
+    func getFriends() -> [Person] {
+        if friends == nil {
+            if let storedData = NSUserDefaults.standardUserDefaults().objectForKey("friends") as? NSData {
+                friends = NSKeyedUnarchiver.unarchiveObjectWithData(storedData) as [Person]?
+            } else {
+                friends = [Person]()
+            }
+        }
+        return friends!
+    }
+    
+    func setFriends(newFriends: [Person]) {
+        println("setFriends")
+        for friend in newFriends {
+            println("\(friend.name)")
+        }
+        let data = NSKeyedArchiver.archivedDataWithRootObject(newFriends)
+        NSUserDefaults.standardUserDefaults().setObject(data, forKey: "friends")
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        // Get an instance of the AVCaptureDevice class to initialize a device object and provide the video
+        // as the media type parameter.
+        let captureDevice = AVCaptureDevice.defaultDeviceWithMediaType(AVMediaTypeVideo)
+        
+        // Get an instance of the AVCaptureDeviceInput class using the previous device object.
+        var error:NSError?
+        let input: AnyObject! = AVCaptureDeviceInput.deviceInputWithDevice(captureDevice, error: &error)
+        
+        if (error != nil) {
+            // If any error occurs, simply log the description of it and don't continue any more.
+            println("\(error?.localizedDescription)")
+            return
+        }
+        
+        // Initialize the captureSession object.
+        captureSession = AVCaptureSession()
+        // Set the input device on the capture session.
+        captureSession?.addInput(input as AVCaptureInput)
+        
+        // Initialize a AVCaptureMetadataOutput object and set it as the output device to the capture session.
+        let captureMetadataOutput = AVCaptureMetadataOutput()
+        captureSession?.addOutput(captureMetadataOutput)
+        
+        // Set delegate and use the default dispatch queue to execute the call back
+        captureMetadataOutput.setMetadataObjectsDelegate(self, queue: dispatch_get_main_queue())
+        captureMetadataOutput.metadataObjectTypes = [AVMetadataObjectTypeQRCode]
+        
+        // Initialize the video preview layer and add it as a sublayer to the viewPreview view's layer.
+        videoPreviewLayer = AVCaptureVideoPreviewLayer(session: captureSession)
+        videoPreviewLayer?.videoGravity = AVLayerVideoGravityResizeAspectFill
+        videoPreviewLayer?.frame = view.layer.bounds
+        view.layer.addSublayer(videoPreviewLayer)
+        
+        // Start video capture.
+        captureSession?.startRunning()
+        
+        // Move the message label to the top view
+        view.bringSubviewToFront(qrcodefriend)
+        
+        // Initialize QR Code Frame to highlight the QR code
+        qrCodeFrameView = UIView()
+        qrCodeFrameView?.layer.borderColor = UIColor.greenColor().CGColor
+        qrCodeFrameView?.layer.borderWidth = 2
+        view.addSubview(qrCodeFrameView!)
+        view.bringSubviewToFront(qrCodeFrameView!)
+
+        
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    func captureOutput(captureOutput: AVCaptureOutput!, didOutputMetadataObjects metadataObjects: [AnyObject]!, fromConnection connection: AVCaptureConnection!) {
+        
+        // Check if the metadataObjects array is not nil and it contains at least one object.
+        if metadataObjects == nil || metadataObjects.count == 0 {
+            qrCodeFrameView?.frame = CGRectZero
+            qrcodefriend.text = "No QR code is detected"
+            return
+        }
+        
+        // Get the metadata object.
+        let metadataObj = metadataObjects[0] as AVMetadataMachineReadableCodeObject
+        
+        if metadataObj.type == AVMetadataObjectTypeQRCode {
+            // If the found metadata is equal to the QR code metadata then update the status label's text and set the bounds
+            let barCodeObject = videoPreviewLayer?.transformedMetadataObjectForMetadataObject(metadataObj as AVMetadataMachineReadableCodeObject) as AVMetadataMachineReadableCodeObject
+            qrCodeFrameView?.frame = barCodeObject.bounds;
+            
+            if metadataObj.stringValue != nil {
+                qrcodefriend.text = metadataObj.stringValue
+            }
+        }
+        let barCodeObject = videoPreviewLayer?.transformedMetadataObjectForMetadataObject(metadataObj as AVMetadataMachineReadableCodeObject) as AVMetadataMachineReadableCodeObject
+        
+        qrCodeFrameView?.frame = barCodeObject.bounds
+    }
+    
+}
+
